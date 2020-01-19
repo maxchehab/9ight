9650891705fb0e0719b8c8969099806cada63db0
maxchehab <maxchehab@gmail.com>
2020-01-19 01:33:27 +0000

    update comments

--- README.md
+++ README.md
@@ -1,9 +1,33 @@
-# Kit
+# Spank
 
-```
-yarn
-yarn dev
-```
+A Next.js or Node Lambda Framework
 
-Library is in `lib/`.
-Example api is in `api/`.
+```ts
+import { NextApiRequest, NextApiResponse } from 'next';
+import { Get, Lambda, Post } from 'spank';
+
+interface User {
+  name: string;
+  id: number;
+}
+
+const UserDB: Array<User> = [];
+
+class Users {
+  @Get()
+  list(req: NextApiRequest, res: NextApiResponse) {
+    return res.json(UserDB);
+  }
+
+  @Post()
+  create(req: NextApiRequest, res: NextApiResponse) {
+    const user = { id: UserDB.length, name: req.body.name };
+
+    UserDB.push(user);
+
+    return res.status(201).json(user);
+  }
+}
+
+export default Lambda(Users);
+```
--- src/common/decorators/request-mapping.decorators.ts
+++ src/common/decorators/request-mapping.decorators.ts
@@ -17,46 +17,46 @@ function createRequestMappingDecorator(method: RequestMethod) {
 }
 
 /**
- * Route handler (method) Decorator. Routes HTTP CONNECT requests to the specified path.
+ * Route handler (method) Decorator. Routes HTTP CONNECT requests to the specified function.
  */
 export const Connect = createRequestMappingDecorator(RequestMethod.CONNECT);
 
 /**
- * Route handler (method) Decorator. Routes HTTP DELETE requests to the specified path.
+ * Route handler (method) Decorator. Routes HTTP DELETE requests to the specified function.
  */
 export const Delete = createRequestMappingDecorator(RequestMethod.DELETE);
 
 /**
- * Route handler (method) Decorator. Routes HTTP GET requests to the specified path.
+ * Route handler (method) Decorator. Routes HTTP GET requests to the specified function.
  */
 export const Get = createRequestMappingDecorator(RequestMethod.GET);
 
 /**
- * Route handler (method) Decorator. Routes HTTP HEAD requests to the specified path.
+ * Route handler (method) Decorator. Routes HTTP HEAD requests to the specified function.
  */
 export const Head = createRequestMappingDecorator(RequestMethod.HEAD);
 
 /**
- * Route handler (method) Decorator. Routes HTTP OPTIONS requests to the specified path.
+ * Route handler (method) Decorator. Routes HTTP OPTIONS requests to the specified function.
  */
 export const Options = createRequestMappingDecorator(RequestMethod.OPTIONS);
 
 /**
- * Route handler (method) Decorator. Routes HTTP PATCH requests to the specified path.
+ * Route handler (method) Decorator. Routes HTTP PATCH requests to the specified function.
  */
 export const Patch = createRequestMappingDecorator(RequestMethod.PATCH);
 
 /**
- * Route handler (method) Decorator. Routes HTTP POST requests to the specified path.
+ * Route handler (method) Decorator. Routes HTTP POST requests to the specified function.
  */
 export const Post = createRequestMappingDecorator(RequestMethod.POST);
 
 /**
- * Route handler (method) Decorator. Routes HTTP PUT requests to the specified path.
+ * Route handler (method) Decorator. Routes HTTP PUT requests to the specified function.
  */
 export const Put = createRequestMappingDecorator(RequestMethod.PUT);
 
 /**
- * Route handler (method) Decorator. Routes HTTP TRACE requests to the specified path.
+ * Route handler (method) Decorator. Routes HTTP TRACE requests to the specified function.
  */
 export const Trace = createRequestMappingDecorator(RequestMethod.TRACE);

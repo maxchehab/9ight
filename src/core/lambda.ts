a312ade9c6a5432ee45dc29c8d20af4b96f8115e
maxchehab <maxchehab@gmail.com>
2020-01-31 05:56:10 +0000

    add default logger

--- src/core/lambda.ts
+++ src/core/lambda.ts
@@ -1,10 +1,11 @@
 import { NextApiResponse, NextApiRequest } from 'next';
-import { generateUrlFromMethods } from '../common/utils';
+import { print, stringify } from 'q-i';
 
 import { ClassType, DecoratorTarget } from '../common/interfaces';
+import { generateUrlFromMethods } from '../common/utils';
 import { HttpException } from '../common/exceptions/http.exception';
-import { RequestMethod, findMethodAndParams } from '../common';
 import { NotFoundException } from '../common/exceptions/not-found.exception';
+import { RequestMethod, findMethodAndParams } from '../common';
 
 export type LambdaFunction = (
   req: NextApiRequest | boolean,
@@ -25,66 +26,92 @@ export function Lambda<t>(
   lambda.__9ight__isLambda = true;
 
   return async (req: NextApiRequest | boolean, res: NextApiResponse) => {
-    try {
-      if (typeof req === 'boolean') {
-        return lambda;
-      }
+    const timestamp = new Date().toUTCString();
+    const target = await executeRequest(req, res, lambda);
 
-      const requestMethod: string = req.method?.toUpperCase();
-      const methods = lambda.__9ight__methods?.filter(
-        ({ method }) => method === requestMethod,
-      );
-
-      const url = generateUrlFromMethods(methods, req.url);
-      const [method, params] = findMethodAndParams(url, methods);
-      const property = method?.property;
-      (req as any).params = params;
-
-      if (!property) {
-        throw new NotFoundException();
-      }
-
-      const args = new Array();
-      const parameters = lambda.__9ight__methodParameters?.get(property) || [];
-
-      for await (const param of parameters) {
-        args[param.index] = await param.transform(req, res);
-      }
-
-      const response = await (lambda as any)[property](...args);
-
-      if (res.headersSent) {
-        return;
-      }
-
-      let statusCode = 200;
-
-      switch (requestMethod) {
-        case RequestMethod.POST: {
-          statusCode = 201;
-          break;
-        }
-      }
-
-      res.status(statusCode);
-
-      if (typeof response === 'object') {
-        return res.json(response);
-      }
-
-      return res.send(String(response));
-    } catch (error) {
-      console.trace(error);
-
-      const exception: HttpException = error;
-
-      if (exception.isHttpException) {
-        return res
-          .status(exception.statusCode)
-          .json({ code: exception.code, message: exception.message });
-      }
-
-      return res.status(500).json({ code: 'Internal Server Error' });
+    if (typeof req === 'boolean') {
+      return target;
     }
+
+    const request = {
+      url: req.url,
+      method: req.method,
+      headers: req.headers,
+    };
+
+    const response = {
+      status: res.statusCode,
+      headers: res.getHeaders(),
+    };
+
+    console.log(stringify({ timestamp, request, response }));
   };
+}
+
+async function executeRequest(
+  req: NextApiRequest | boolean,
+  res: NextApiResponse,
+  lambda: DecoratorTarget,
+) {
+  try {
+    if (typeof req === 'boolean') {
+      return lambda;
+    }
+
+    const requestMethod: string = req.method?.toUpperCase();
+    const methods = lambda.__9ight__methods?.filter(
+      ({ method }) => method === requestMethod,
+    );
+
+    const url = generateUrlFromMethods(methods, req.url);
+    const [method, params] = findMethodAndParams(url, methods);
+    const property = method?.property;
+    (req as any).params = params;
+
+    if (!property) {
+      throw new NotFoundException();
+    }
+
+    const args = new Array();
+    const parameters = lambda.__9ight__methodParameters?.get(property) || [];
+
+    for await (const param of parameters) {
+      args[param.index] = await param.transform(req, res);
+    }
+
+    const response = await (lambda as any)[property](...args);
+
+    if (res.headersSent) {
+      return;
+    }
+
+    let statusCode = 200;
+
+    switch (requestMethod) {
+      case RequestMethod.POST: {
+        statusCode = 201;
+        break;
+      }
+    }
+
+    res.status(statusCode);
+
+    if (typeof response === 'object') {
+      return res.json(response);
+    }
+
+    return res.send(String(response));
+  } catch (error) {
+    console.trace(error);
+
+    const exception: HttpException = error;
+
+    if (exception.isHttpException) {
+      return res
+        .status(exception.statusCode)
+        .json({ code: exception.code, message: exception.message });
+    }
+
+    return res.status(500).json({ code: 'Internal Server Error' });
+  }
 }
